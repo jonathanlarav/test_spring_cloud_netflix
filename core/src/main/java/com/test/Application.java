f7fd4106dd2a8729d91841a5a6de752047212ae3
jonathan lara <jonathan.lara@senioradvisor.com>
2015-10-13 14:15:20 -0500

    adding spring-cloud Eureka client/server configuration

---
+++ core/src/main/java/com/test/Application.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by jonathanlara on 10/13/15.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
