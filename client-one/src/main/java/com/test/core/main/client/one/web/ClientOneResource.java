aba015532ca227e5e0d1984a5d531fa2e685bcde
Keith Lopez <klopez@keiths-mbp.gateway.pace.com>
2015-10-16 13:24:12 -0500

    Inject properties into clients one and two from configuration server

--- client-one/src/main/java/com/test/core/main/client/one/web/ClientOneResource.java
+++ client-one/src/main/java/com/test/core/main/client/one/web/ClientOneResource.java
@@ -1,6 +1,7 @@
 package com.test.core.main.client.one.web;
 
 import com.test.core.main.client.one.model.Message;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientOneResource {
 
+    @Value("${I.am}")
+    private String iAm;
+
+    @Value("${I.have}")
+    private String iHave;
+
+    @Value("${I.look}")
+    private String iLook;
+
     @RequestMapping("/message/supplier")
     public Message home(@RequestParam("message") String message) {
         return new Message("Hello, this is your Message: " + message);
+    }
+
+    @RequestMapping("/whatAmI")
+    public String whatAmI() {
+        return iAm + " and " + iHave + " and " + iLook;
     }
 }
